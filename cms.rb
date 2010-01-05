cd6ebc1e03d1f601fadb2d76ccfec1ea4793f755
Matt Dean <mattdean@mattdean-mbp.local>
2010-01-04 22:34:24 -0500

    Updating templates with latest additions to environment.rb

--- cms.rb
+++ cms.rb
@@ -167,6 +167,15 @@ Gem.loaded_specs.values.each do |spec|
     require init_path if File.exists?(init_path)
   end
 end
+
+ActionController::Dispatcher.middleware.insert_before(
+  ActionController::Session::CookieStore,
+  FlashSessionCookieMiddleware,
+  ActionController::Base.session_options[:key]
+)
+
+CMS.start
+
 }
 
 file 'app/controllers/application_controller.rb', %{
